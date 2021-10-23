03edcb01f02da803200f1b1bf87ff371fd88fa3f
Yana <yanaegorova69.3@gmail.com>
2021-10-23 22:41:51 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,326 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Wizarding Wand',
+        price: 19.99,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Official Harry Potter Wand',
+                text: 'The Harry Potter Spellbinding Wand is a must-have for your collection! Pretend to cast Wizarding World spells and collect your favorite character’s wands (each sold separately)!',
+            },
+            {
+                title: 'Authentic Details',
+                text: 'This authentic 12-inch replica Harry Potter wand is highly detailed with a weighted base, making it easy to hold. It’s the perfect accessory for your Wizarding World adventures!',
+            },
+            {
+                title: 'Harry Potter Wands For Ages 6 And Up',
+                text: 'These Harry Potter accessories are a great birthday, holiday, or Christmas gift for kids. Learn your favorite Wizarding World spells with the Spellbinding Wands (each sold separately)!',
+            },
+            {
+                title: 'Harry Potter Gifts And Toys For 6 Year Old Boys And Girls',
+                text: 'Discover the Wizarding World with Harry Potter dolls, wands, toy figures & playsets! Harry Potter toys for kids are the perfect gifts!',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Batman Toy',
+        price: 24.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Designed with Ultra Articulation with up to 22 moving parts for full range of posing and play.',
+            },
+            {
+                text: 'Batman is based on his look from the DC Comic Dark Knight Returns.',
+            },
+            {
+                text: 'Comes with alternate hands, a Batarang with rope and base.',
+            },
+            {
+                text: 'Figure includes 1 of 4 pieces that combine with The Dark Knight Returns Build-a line to assemble the Horse Batman rides into battle from the Dark Knight Returns.',
+            },
+            {
+                text: 'Includes collectible art card with figure photography on the front, and character biography on the back.',
+            },
+            {
+                text: 'Figure style may vary as an ultra rare chase figure may be received.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png', './img/img2-5.png', './img/img2-6.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Dinosaur Figure',
+        price: 35.32,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'This larger-scale dinosaur action figure features a dynamic claw slashing feature, a chomp and roar plus a mighty tail whip attack!',
+            },
+            {
+                text: 'Push the first button to start a full-on arm extending slash and grab attack.',
+            },
+            {
+                text: 'Activate a big chomp and roar by pressing a second button. Pull the tail back to activate the spring-loaded motion for a surprise tail whip attack!',
+            },
+            {
+                text: 'It\'s the perfect gift for ages 4 years old and up especially Jurassic World fans and those who love dinosaurs and lively action play!',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Magnetic Drawing Board',
+        price: 39.99,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'Colorful Magnetic Drawing Board',
+                text: 'This magnetic drawing board table has 4 color areas: red, blue, green and yellow. Colorful magna erasable doodle board is helpful to educate children to learn color, and cultivate their ability of writing and painting. Develop children\'s creative imagination. Besides painting, toddlers can exercise arithmetic and writing on the drawing board.',
+            },
+            {
+                title: 'Adorable Magnetic Doodle Board',
+                text: 'Magnetic doodle board table with non-toxic, high-quality ABS material. The drawing board with 2 stamps features round edges and soft curves to eliminate potential injuries. The design of the handle is convenient for children to carry, and the four legs of the doodle board table are solid enough to support the drawing board. Magic writing pen with strong string to prevent it from being lost.',
+            },
+            {
+                title: 'Magna Erasable Sketch Board',
+                text: 'The doodle board can be used repeatedly. You only need to gently pull the slide back and forth, and it will be clean again. Extend the life of the toy and let your child love it all the time. The drawing board of this table is 4.9*7.3 inches in drawing area; great size suitable for preschool children to play. Magnetic writing board keeps children\'s hands clean.',
+            },
+            {
+                title: 'Magnetic Doodle for Toddlers',
+                text: 'The four legs of the drawing table are very convenient for installation and disassembly, and change the form of the magnetic writing board: upright, oblique and flat. Children can choose the most suitable form for their own drawing according to their own needs. It helps to develop the correct sitting posture of children.',
+            },
+            {
+                title: 'Toddler Toys for Boys Girls Age 2-5',
+                text: 'Magnetic drawing board is great toys for girls age 1 2 3 4 5 6 .Little kids should use under adult’s supervision. A playing toy for your kids, enjoy endless fun for indoor and outdoor travel. Festivals gifts, birthday gift or visit gift and suit for toddlers. Funny toys for children, kids love it.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Paintable Wooden Doll House',
+        price: 43.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Mansion doll house features 3 bedroom 1 Living Room 1 balcony and 15 pieces of unique wood furniture.',
+            },
+            {
+                text: 'Fun Toy : Princess doll house creates new worlds of imaginations with this classic toy house.Size 21 × 21 × 9.',
+            },
+            {
+                text: 'Paintable ; Perfect wood painting crafts for loved ones.Paint sold separately. Acrylic paints recommended.',
+            },
+            {
+                text: 'Creativity for Girls and Boys Birthday Christmas New Year Gifts for who enjoy learning activities.',
+            },
+            {
+                text: 'Craft Kit 360 Degree Play paintable wooden townhouse for toddler to develop creative game.',
+            },
+            {
+                text: 'Imaginative Play Building and Painting wood dollhouse will help development of kids imagination.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Doll',
+        price: 49.95,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'These classic 12 inch baby dolls are perfect for tiny hands; Made with soft bodies meant to be hugged, these dolls are recommended for ages 2 plus; This adorable dolly will become your baby’s baby.',
+            },
+            {
+                title: 'Details',
+                text: 'Wears a mint and white checked knit onesie with long sleeves, covered feet and a Peter Pan collar; Ribbon on the front and lace trim at the wrists and collar; Features blue eyes and light brown painted hair.',
+            },
+            {
+                title: 'Love Is In The Details',
+                text: 'Our baby dolls are made to be played with safely and comfortably, with soft bodies and nontoxic materials. Realistic details like soft eye lashes are crafted to delight.',
+            },
+            {
+                title: 'Empowering Girls',
+                text: 'Kids\' toys like the lifelike dolls we offer inspire creativity, nurturing, and kindness.',
+            },
+            {
+                text: 'A matching pink hat ties beneath her chin and is ornamented with flower embroidery and lace trim.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png','./img/img6-4.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Character Chair',
+        price: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'Soft sit-in seat and sturdy design for hours of safe playtime fun.',
+            },
+            {
+                text: 'Comes fully assembled (so kids can jump right on and into their imaginative play).',
+            },
+            {
+                text: 'Additional characters (so everyone can be the hit at the baby shower or first birthday – no matter the theme!).',
+            },
+            {
+                text: 'The perfect size (28”L x 12”W x 22”H) and super cushy.',
+            },
+            {
+                text: 'Ride in seat: withstands a maximum weight of 75 lbs.',
+            },
+            {
+                text: 'Kids 18 months+: Our character furniture is suitable for children ages 18 months and older.',
+            },
+            {
+                text: 'Spot clean: Damp cloth allows for a quick surface wash.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png', './img/img7-4.jpg'],
+    },
+    {
+        id: 'product-8',
+        name: 'Play Kitchen Set',
+        price: 79.99,
+        image: './img/img8.png',
+        description: [
+            {
+                title: 'Fully Equipped Gourmet Kitchen',
+                text: 'This wooden play kitchen has all a little cook needs with a sink, stove, oven with window, cabinet, spice shelf, and turn able clicking knobs.',
+            },
+            {
+                title: 'Master Chef Dreams',
+                text: 'Gourmet kitchen stimulates creative thinking and storytelling, your little chef will be enchanted as they cook up a storm.',
+            },
+            {
+                title: 'Real Life Kitchen',
+                text: 'This kids play kitchen encourages kinesthetic development with its realistic features of clickable, turn able knobs and switch, transparent oven window and handle shapes.',
+            },
+            {
+                title: 'Durability',
+                text: 'Kitchen and grill Playset is made from durable wood, child safe materials and non-toxic water-based paint.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Skateboard',
+        price: 84.41,
+        image: './img/img9.png',
+        description: [
+            {
+                title: 'Deck',
+                text: 'Lightweight & strong 7-ply birch construction.',
+            },
+            {
+                title: 'Trucks',
+                text: 'Strong, lightweight cast aluminum trucks w/ either 85a cushions (super-micro, micro, & mini) or 90a cushions.',
+            },
+            {
+                title: 'Grip Tape',
+                text: 'High quality professional grade grit and adhesive.',
+            },
+            {
+                title: 'Bearings',
+                text: 'Durable high speed precision steel bearings with oil lubricant for easy rolling.',
+            },
+            {
+                text: '8.25 x 31.5, 14 Inch Wheelbase.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'My Size Elsa Doll',
+        price: 89.99,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'Ice Powers Playdate Elsa is inviting you to join her on her action-packed adventure to find Ahtohallan.',
+            },
+            {
+                text: 'Elsa features Ice power sounds and snowflake lights that run down her arms and out her palms.',
+            },
+            {
+                text: 'Says 12 film-inspired phrases and is dressed in her stunning film-inspired Dark Sea dress.',
+            },
+            {
+                text: 'Lightweight, and with 11 points of articulation, child-sized Ice Powers Playdate Elsa can sit on her trusted companion, Disney’s Frozen 2 Playdate Water Nokk (sold separately), Ice Powers Playdate Elsa can even transform Playdate Water Nokk through her touch.',
+            },
+            {
+                text: 'Includes: 1 Doll, 1 Doll Dress - Requires 3 x AA batteries (included) - Suggested for Ages 3+.',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png', './img/img10-4.png', './img/img10-5.png', './img/img10-6.png', './img/img10-7.png', './img/img10-8.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Soft Foam Playset',
+        price: 119.98,
+        image: './img/img11.png',
+        description: [
+            {
+                title: 'Multi-Purpose Playset',
+                text: 'Configurable climber with slide, speed bump and soft block play pieces great for infants exploring new textures and learning to grasp and play; foldable half moon speed bump becomes a seat perfect for story time; for the crawler.',
+            },
+            {
+                title: 'Ideal For In-Home And Institutional Learning',
+                text: 'High-quality, versatile climber incorporates safe indoor active play into any learning environment, from home to classroom, and anywhere in between.',
+            },
+            {
+                title: 'Gross Motor Development',
+                text: 'Encourages little ones to develop important growth skills, hand-eye coordination and social skills as they play.',
+            },
+            {
+                title: 'Non-Slip Base',
+                text: 'Textured non-slip bottom reduces slipping and sliding.',
+            },
+            {
+                title: 'Safe For Your Space',
+                text: 'GREENGUARD [GOLD] and CertiPUR-US certified for low emissions into indoor air; complies with CAL 117 flammability regulations.',
+            },
+            {
+                title: 'Easy To Maintain',
+                text: 'Soft, durable polyurethane material is easy to clean; wipe with a mild soap-and-water solution; children should remove shoes before use.',
+            },
+            {
+                title: 'Product Specifications',
+                text: 'Recommended for kids 6 months — 24 months, adult supervision required; includes 6 pieces in different shapes and sizes; largest piece measures 24"L x 16" W x 8"H; no assembly required.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Kid\'s Tricycle',
+        price: 148.00,
+        image: './img/img12.png',
+        description: [
+            {
+                text: '4-in-1 trike adjusts as your child grows. For 10 months+ to 44 pounds.',
+            },
+            {
+                text: 'Extra-large 10" front wheel for easy mobility on several terrains.',
+            },
+            {
+                text: 'Parent push handle is removable and height adjustable from 32.5 to 38 inches.',
+            },
+            {
+                text: '3-position adjustable seat accommodates a growing child, shoulder and seat pads are removable and washable.',
+            },
+            {
+                text: 'Large UVF 50 canopy provides sun protection, includes a cup holder and rear storage basket.',
+            },
+        ],
+        images: ['./img/img12-1.png','./img/img12-2.png'],
+    },
+]
